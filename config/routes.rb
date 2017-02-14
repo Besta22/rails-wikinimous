71f9e24d00b03c8858834ad0d3dd0d127fd94ef2
Elissa Besta <elissa.besta@gmail.com>
2017-02-14 17:27:26 +0000

    Works and looks a little better.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+    root to: 'articles#index'
+  # get 'articles/create'
+
+  # get 'articles/destroy'
+
+  # get 'articles/edit'
+
+  # get 'articles/index'
+
+  # get 'articles/new'
+
+  # get 'articles/show'
+  resources :articles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,13 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
-#
+#Faker::GameOfThrones
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do
+  article = Article.new({
+      name: Faker::GameOfThrones.character,
+      body: Faker::Hipster.paragraph
+    })
+  article.save
+end
